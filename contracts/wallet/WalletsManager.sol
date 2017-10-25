ca0a1bfeeeffc63ae7478fd077b6fec58f607805
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2017-10-25 12:36:15 +0300

    MINT-582 Improve walletManager and wallet contracts. Code formatting. Add missed function visibility specifiers. Explicitly Init `storage` fields. Make `create2FAWallet` / `createWallet` / `AddWallet` available only for users deposited TIMEs.

--- contracts/wallet/WalletsManager.sol
+++ contracts/wallet/WalletsManager.sol
@@ -19,7 +19,6 @@ contract MultiEventsHistoryInterface {
 }
 
 contract WalletsManager is WalletsManagerEmitter, FeatureFeeAdapter, BaseManager {
-
     uint constant ERROR_WALLET_INVALID_INVOCATION = 14000;
     uint constant ERROR_WALLET_EXISTS = 14001;
     uint constant ERROR_WALLET_OWNER_ONLY = 14002;
@@ -27,43 +26,46 @@ contract WalletsManager is WalletsManagerEmitter, FeatureFeeAdapter, BaseManager
     uint constant ERROR_WALLET_UNKNOWN = 14004;
 
     StorageInterface.OrderedAddressesSet wallets;
-
     StorageInterface.Address walletsFactory;
-
     StorageInterface.Address oracleAddress;
-
     StorageInterface.UInt oraclePrice;
 
     function WalletsManager(Storage _store, bytes32 _crate) BaseManager(_store, _crate) {
         walletsFactory.init("walletsFactory");
         wallets.init('wallets');
+        oracleAddress.init('oracleAddress');
+        oraclePrice.init('oraclePrice');
     }
 
-    function init(address _contractsManager, address _walletsFactory) onlyContractOwner returns (uint) {
+    function init(address _contractsManager, address _walletsFactory)
+    public
+    onlyContractOwner
+    returns (uint)
+    {
         BaseManager.init(_contractsManager, "WalletsManager");
         store.set(walletsFactory, _walletsFactory);
         return OK;
     }
 
-    function kill(address[] tokens) onlyAuthorized returns (uint) {
+    function kill(address[] tokens)
+    public
+    onlyContractOwner
+    returns (uint)
+    {
         withdrawnTokens(tokens,msg.sender);
         selfdestruct(msg.sender);
         return OK;
     }
 
-    function getOraclePrice() constant returns (uint) {
+    function getOraclePrice() public constant returns (uint) {
         return store.get(oraclePrice);
     }
 
-    function getOracleAddress() constant returns (address) {
+    function getOracleAddress() public constant returns (address) {
         return store.get(oracleAddress);
     }
 
-    function isWalletOwner(address _wallet, address _owner) internal returns (bool) {
-        return WalletInterface(_wallet).isOwner(_owner);
-    }
-
-    function getWallets() constant returns (address[] result, bytes32[] result2, bool[] result3) {
+    function getWallets() public constant returns (address[] result, bytes32[] result2, bool[] result3) {
         StorageInterface.Iterator memory iterator = store.listIterator(wallets);
         address wallet;
         result = new address[](store.count(wallets));
@@ -90,8 +92,38 @@ contract WalletsManager is WalletsManagerEmitter, FeatureFeeAdapter, BaseManager
         return OK;
     }
 
-    function addWallet(address _wallet) returns (uint) {
+    function createWallet(address[] _owners, uint _required, bytes32 _name, uint _releaseTime)
+    public
+    returns (uint errorCode)
+    {
+        return _createWallet(_owners, _required, _name, _releaseTime, [uint(0)]);
+    }
+
+    function create2FAWallet(bytes32 _name, uint _releaseTime)
+    public
+    returns (uint errorCode)
+    {
+        return _create2FAWallet(_name, _releaseTime, [uint(0)]);
+    }
+
+    /**
+    *  Deletes sender from a list of wallets if present. Designed to be called only by wallet.
+    */
+    function removeWallet() public returns (uint) {
+        if(!store.includes(wallets,msg.sender)) {
+            return _emitError(ERROR_WALLET_UNKNOWN);
+        }
+
+        store.remove(wallets,msg.sender);
+        return OK;
+    }
+
+    function addWallet(address _wallet) public returns (uint) {
         return _addWallet(_wallet, [uint(0)]);
+    }
+
+    function isWalletOwner(address _wallet, address _owner) internal returns (bool) {
+        return WalletInterface(_wallet).isOwner(_owner);
     }
 
     function _addWallet(address _wallet, uint[1] memory _result)
@@ -118,21 +150,6 @@ contract WalletsManager is WalletsManagerEmitter, FeatureFeeAdapter, BaseManager
         return OK;
     }
 
-    function removeWallet() returns (uint) {
-        if(store.includes(wallets,msg.sender)) {
-            store.remove(wallets,msg.sender);
-            return OK;
-        }
-        return _emitError(ERROR_WALLET_UNKNOWN);
-    }
-
-    function createWallet(address[] _owners, uint _required, bytes32 _name, uint _releaseTime)
-    public
-    returns (uint errorCode)
-    {
-        return _createWallet(_owners, _required, _name, _releaseTime, [uint(0)]);
-    }
-
     function _createWallet(address[] _owners, uint _required, bytes32 _name, uint _releaseTime, uint[1] memory _result)
     private
     featured(_result)
@@ -146,13 +163,6 @@ contract WalletsManager is WalletsManagerEmitter, FeatureFeeAdapter, BaseManager
 
         _result[0] = OK;
         return OK;
-    }
-
-    function create2FAWallet(bytes32 _name, uint _releaseTime)
-    public
-    returns (uint errorCode)
-    {
-        return _create2FAWallet(_name, _releaseTime, [uint(0)]);
     }
 
     function _create2FAWallet(bytes32 _name, uint _releaseTime, uint[1] memory _result)
@@ -185,8 +195,7 @@ contract WalletsManager is WalletsManagerEmitter, FeatureFeeAdapter, BaseManager
         WalletsManager(getEventsHistory()).emitWalletCreated(wallet);
     }
 
-    function()
-    {
+    function() {
         revert();
     }
 }
--- contracts/wallet/WalletsManagerEmitter.sol
+++ contracts/wallet/WalletsManagerEmitter.sol
@@ -3,21 +3,19 @@ pragma solidity ^0.4.11;
 import '../core/event/MultiEventsHistoryAdapter.sol';
 
 contract WalletsManagerEmitter is MultiEventsHistoryAdapter {
-
     event Error(address indexed self, uint errorCode);
     event WalletAdded(address indexed self, address wallet);
     event WalletCreated(address indexed self, address wallet);
 
-    function emitError(uint errorCode) {
+    function emitError(uint errorCode) public {
         Error(_self(), errorCode);
     }
 
-    function emitWalletAdded(address wallet) {
+    function emitWalletAdded(address wallet) public {
         WalletAdded(_self(), wallet);
     }
 
-    function emitWalletCreated(address wallet) {
+    function emitWalletCreated(address wallet) public {
         WalletCreated(_self(), wallet);
     }
-
 }

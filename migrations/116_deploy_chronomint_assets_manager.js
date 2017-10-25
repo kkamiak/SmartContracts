89d3037d1f8704154bee3f0d21266d96909bb8d3
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2017-10-25 20:19:19 +0300

    Link library to AssetsManager in migrations

--- migrations/116_deploy_chronomint_assets_manager.js
+++ migrations/116_deploy_chronomint_assets_manager.js
@@ -1,8 +1,11 @@
 var AssetsManager = artifacts.require("./AssetsManager.sol");
 const Storage = artifacts.require('./Storage.sol');
+const AssetsManagerAggregations = artifacts.require('./AssetsManagerAggregations.sol')
 
 module.exports = function (deployer, network) {
-    deployer.deploy(AssetsManager, Storage.address, 'AssetsManager')
+    deployer
+    .then(() => deployer.link(AssetsManagerAggregations, AssetsManager))
+    .then(() => deployer.deploy(AssetsManager, Storage.address, 'AssetsManager'))
 
     .then(() => console.log("[MIGRATION] [" + parseInt(require("path").basename(__filename)) + "] AssetsManager deploy: #done"))
 }

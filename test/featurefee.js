51070990a99882c6d1a1ed184779c0c4e1b333ad
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2017-10-25 19:50:47 +0300

    Add negative test to be sure that the fee does not taken if executed method is failed

--- test/featurefee.js
+++ test/featurefee.js
@@ -25,6 +25,9 @@ contract("FeatureFeeManager", function(accounts) {
     const CreateWalletFeatureRequiredBalance = 2;
     const CreateWalletFeatureFee = 1;
 
+    const AddWalletFeatureRequiredBalance = 4;
+    const AddWalletFeatureFee = 3;
+
     before("setup", function(done) {
         FeatureFeeManager.deployed()
         .then(_featureFeeManager => featureFeeManager = _featureFeeManager)
@@ -42,15 +45,20 @@ contract("FeatureFeeManager", function(accounts) {
         .then((_assetDonator) => assetDonator = _assetDonator)
 
         .then(() => WalletsManager.deployed())
-        .then((_walletsManager) => {
-            walletsManager = _walletsManager;
+        .then((_walletsManager) => walletsManager = _walletsManager)
+        .then(() => {
             let sig = walletsManager.contract.createWallet.getData([0],0,0,0).slice(0, 10);
             return featureFeeManager.setFeatureFee(WalletsManager.address, sig, CreateWalletFeatureRequiredBalance, CreateWalletFeatureFee);
         })
 
+        .then(() => {
+            let sig = walletsManager.contract.addWallet.getData(0).slice(0, 10);
+            return featureFeeManager.setFeatureFee(WalletsManager.address, sig, AddWalletFeatureRequiredBalance, AddWalletFeatureFee);
+        })
+
         .then(() => assetDonator.sendTime({from: timeHolder1}))
-        .then(() => TIME.approve(timeHolderWallet, CreateWalletFeatureRequiredBalance, {from: timeHolder1}))
-        .then(() => timeHolder.deposit(CreateWalletFeatureRequiredBalance, {from : timeHolder1}))
+        .then(() => TIME.approve(timeHolderWallet, 10000, {from: timeHolder1}))
+        .then(() => timeHolder.deposit(10000, {from : timeHolder1}))
         .then(() => Setup.setup(done))
     })
 
@@ -98,6 +106,29 @@ contract("FeatureFeeManager", function(accounts) {
     it("should allow to execute WalletsManager#addWallet() account with deposit > feature_price")
 
     it("should not allow to execute WalletsManager#addWallet() account with deposit < feature_price")
+
+    it("should not take fee if WalletsManager#addWallet() execution is failed",  async () => {
+        let holderBalance;
+        let walletAddress;
+
+        return Promise.resolve()
+        .then(() => walletsManager.createWallet([timeHolder1, timeHolder2], 1, "Test Wallet 1", 0,  {from :timeHolder1}))
+        .then(tx => eventsHelper.extractEvents(tx, "WalletCreated"))
+        .then(events => walletAddress = events[0].args.wallet)
+
+        .then(() => timeHolder.depositBalance.call(timeHolder1))
+        .then(_balance => {
+            holderBalance = _balance;
+            assert.isTrue(holderBalance >= AddWalletFeatureRequiredBalance)
+        })
+        .then(() => walletsManager.addWallet.call(walletAddress, {from :timeHolder1}))
+        .then(r => assert.equal(r, ErrorsEnum.ERROR_WALLET_EXISTS))
+        .then(() => walletsManager.addWallet(walletAddress,  {from :timeHolder1}))
+        .then(tx => eventsHelper.extractEvents(tx, "Error"))
+        .then(events => assert.equal(events.length, 1))
+        .then(() => timeHolder.depositBalance.call(timeHolder1))
+        .then(_balance => assert.equal(holderBalance.toNumber(), _balance.toNumber()))
+    })
 
     it("should allow to execute TokenManagementInterface#createAssetWithoutFee() account with deposit > feature_price")
 
